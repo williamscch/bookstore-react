c38916db7d07676b7a42c6a267c49cfad4b5abd3
wdavidcch <david.13.2121@gmail.com>
2022-08-17 18:32:28 -0400

    Fix code reviewer recomendations

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import Navigation from './components/Navigation';
 import Categories from './components/Categories';
 import BooksPage from './components/BooksPage';
-import store from './redux/configureStore';
 
 const App = () => (
-  <Provider store={store}>
+  <>
     <Navigation />
     <Routes>
       <Route path="/" element={<BooksPage />} />
       <Route path="/categories" element={<Categories />} />
     </Routes>
-  </Provider>
+  </>
 );
 
 export default App;
